067b3e264bca06970a57355c7b3f2383be99105d
nicholaseasler <nicholaseasler@192.168.1.113>
2014-09-05 12:04:10 -0400

    getting web accessors working

--- api/code/models/gen.sh
+++ api/code/models/gen.sh
@@ -1,4 +1,4 @@
 python orm.py
 #find . -name '*.php' -not -name '*Model.php' -not -name 'repos*php' -exec mv {} ../../ \; -print
 mv *Controller.php ../controllers/
-mv index.php ../
+mv index.php ../../
--- api/index.php
+++ api/index.php
@@ -1,21 +1,51 @@
 <?php
-    require_once('code/startup.php');
-    require_once('restfulSetup.php');
-    
-    if (strlen($_SERVER['REQUEST_URI']) > 1024) exit();
+	require_once ('code/startup.php');
+	require_once ("gameController.php");
+	require_once ("locationController.php");
+	require_once ("categoryController.php");
+	require_once ("userController.php");
 
-    $requestURI = explode('/', $_SERVER['REQUEST_URI']);
-    $scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
+	if (strlen($_SERVER['REQUEST_URI']) > 1024) exit();
 
-    for($i = 0; $i < sizeof ($scriptName); $i++)
-    {
-        if ($requestURI [$i] == $scriptName [$i])
-        {
-            unset($requestURI [$i]);
-        }
-    }
+	$requestURI = explode('/', $_SERVER['REQUEST_URI']);
+	$scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
+	for($i = 0; $i < sizeof ($scriptName); $i++)
+	{
+		if ($requestURI [$i] == $scriptName [$i])
+		{
+			unset($requestURI [$i]);
+		}
+	}
 
-    $command = SanitizeStringArray (array_values ($requestURI));
-    $controller = $command [0];
-    $args = array_slice ($command, 1);
+	$command = SanitizeStringArray (array_values ($requestURI));
+	$controllerName = strtolower ($command [0]);
+	$args = array_slice ($command, 1);
+	$controllerInstance = NULL;
+
+	switch ($controllerName){
+		case "game":
+			$controllerInstance = new GameController();
+			break;
+		case "location":
+			$controllerInstance = new LocationController();
+			break;
+		case "category":
+			$controllerInstance = new CategoryController();
+			break;
+		case "user":
+			$controllerInstance = new UserController();
+			break;
+	}
+
+	if ($controllerInstance !== NULL){
+		switch ($_SERVER["REQUEST_METHOD"]){
+			case "GET":
+				$controllerInstance->get($args);
+				break;
+		}
+	}
+	else{
+		header("HTTP/1.0 404 Not Found");
+	}
+
 ?>
