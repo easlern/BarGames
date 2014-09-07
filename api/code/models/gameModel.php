1fa1cce09791e6d8abc8296609521dcb5303dad9
Ryan Duffing <ryanduffing@gmail.com>
2014-09-07 09:53:59 -0400

    game model overwrite

--- api/code/models/gameModel.php
+++ api/code/models/gameModel.php
@@ -1,34 +1,47 @@
 <?php
-	class game{
+	class Game{
 		private $id;
 		private $locationId;
 		private $name;
 		private $categoryIds;
 
+		function __construct ($id, $locationId, $name, $categoryIds){
+			$this->id = $id;
+			$this->locationId = $locationId;
+			$this->name = $name;
+			$this->categoryIds = $categoryIds;
+		}
+
 		public function getId(){
 			return $this->id;
 		}
-		public function getLocationid(){
+		public function getLocationId(){
 			return $this->locationId;
 		}
 		public function getName(){
 			return $this->name;
 		}
-		public function getCategoryids(){
+		public function getCategoryIds(){
 			return $this->categoryIds;
 		}
 
-		public function setId($value){
-			$this->id = $value;
-		}
-		public function setLocationid($value){
+		public function setLocationId($value){
 			$this->locationId = $value;
 		}
 		public function setName($value){
 			$this->name = $value;
 		}
-		public function setCategoryids($value){
+		public function setCategoryIds($value){
 			$this->categoryIds = $value;
+		}
+
+		public function toJson(){
+			$json = new stdClass();
+			$json->id = $this->getId();
+			$json->locationId = $this->getLocationId();
+			$json->name = $this->getName();
+			$json->categoryIds = $this->getCategoryIds();
+			return json_encode ($json);
 		}
 	}
 ?>
